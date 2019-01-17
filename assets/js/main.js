5912a7636edc3de42bb0867160886ce8536f7b7f
FeliOdras <ariane.feldmann@odras.de>
2019-01-17 11:10:45 +0100

    added line break for console.log and html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 "use strict"
 
 function printTree(loops) {
-    let tree = "*"
-    for (let i = 1; i < loops; i++) {
-        tree += '<br>';
+    let tree = ""
+    for (let i = 0; i < loops; i++) {
+        tree += "<br> \n";
         for (let j = 0; j <= i; j++) {
             tree += "*"
         }
     }
+    console.log(tree);
     return tree;
 }
 document.getElementById('output').innerHTML = printTree(5)
